210925a6b43a422d0cd8d2879547c57d5f2e82a5
Mattia de' Michieli Vitturi <demichie@gmail.com>
2024-10-17 15:57:58 +0200

    Update createWebformDict.py

--- ELICITATIONS/STROMBOLI_2nd/createWebformDict.py
+++ ELICITATIONS/STROMBOLI_2nd/createWebformDict.py
@@ -8,7 +8,7 @@ RepositoryData = 'Stromboli_2nd_answers'
 quest_type = 'target'
 
 
-target_list = [2,3]
+target_list = [36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62]
 # seed_list = [1,2]
 absolute_indexing = True
 
