1e21ee6c4734c629c9655de0944050b6eac5fec4
Mattia de' Michieli Vitturi <demichie@gmail.com>
2024-10-17 14:43:09 +0200

    Update createWebformDict.py

--- ELICITATIONS/STROMBOLI_2nd/createWebformDict.py
+++ ELICITATIONS/STROMBOLI_2nd/createWebformDict.py
@@ -1,18 +1,14 @@
 # select 'github' or 'local'
-datarepo = 'local'
+# datarepo = 'local'
 
-# datarepo = 'github'
-# RepositoryData = 'createWebformAnswers'
-
-Repository = 'testWebform'
+datarepo = 'github'
+RepositoryData = 'Strombli_2nd_answers'
 
 # this can be 'seed' or 'target'
 quest_type = 'target'
 
-#group_list = ['SG1 (Clermont)', 'SG2 (Quito)', 'SG3 (Geol.)', 'SG4 (MathMod)',
-#              'SG5 (Junior)', 'SG6 (Senior)']
 
-# target_list = [2,3]
+target_list = [2,3]
 # seed_list = [1,2]
 absolute_indexing = True
 
