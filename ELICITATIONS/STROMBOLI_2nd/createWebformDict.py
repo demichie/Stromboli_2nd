ac215dee95acc8f5a3d07ca4d0b77e0537d9a3a8
Mattia de' Michieli Vitturi <demichie@gmail.com>
2024-10-17 15:58:21 +0200

    Update createWebformDict.py

--- ELICITATIONS/STROMBOLI_2nd/createWebformDict.py
+++ ELICITATIONS/STROMBOLI_2nd/createWebformDict.py
@@ -10,7 +10,7 @@ quest_type = 'target'
 
 target_list = [36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62]
 # seed_list = [1,2]
-absolute_indexing = True
+absolute_indexing = False
 
 # encrypted = False
 
